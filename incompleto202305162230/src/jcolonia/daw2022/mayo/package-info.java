ac0b44f531c0afe88dd28d5a4337f636b50971d6
Luis G. Contreras <luisg.concon@educa.jcyl.es>
2023-05-17 08:48:39 +0200

    Primera comision. Proyecto empezado.

---
+++ incompleto202305162230/src/jcolonia/daw2022/mayo/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Aplicación de gestión de datos sobre consola de texto y estructura M-V-C:
+ * Agenda. Ejercicio de evaluación sobre la UT6, primera prueba específica
+ * parcial de la E3.
+ * 
+ * @author <a href="dmartin.jcolonia@gmail.com">David H. Martín</a>
+ * @version 1.0 (20230504)
+ */
+package jcolonia.daw2022.mayo;
